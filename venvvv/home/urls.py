f1c3eaa633e06ff836643d8f1b3b6bed44b9f925
Ziad Emad <115287958+1Ziad-Etman1@users.noreply.github.com>
2023-09-23 02:23:04 +0300

    Finished the project

--- venvvv/home/urls.py
+++ venvvv/home/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from home import views
+
+from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('tasks', views.tasks, name='tasks')
+    path('', tasks, name='tasks'),
+    path('addTask', addTask, name='addTask'),
+    path('edit/<int:pk>', Edit.as_view(), name='edit'),
+    path('delete/<int:pk>', Delete.as_view(), name='delete'),
+    path('details/<int:pk>', Details.as_view(), name='details'),
 ]
--- venvvv/home/views.py
+++ venvvv/home/views.py
@@ -1,22 +1,42 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from home.models import Task
+from django.views.generic import DetailView,DeleteView,UpdateView
 
 # Create your views here.
 
-def home(request):
+def addTask(request):
     context = {'success' : False}
     if request.method =='POST':
         title = request.POST['title']
         desc = request.POST['description']
-        print(title, desc)
         ins = Task(taskTitle=title, taskDesc=desc)
         ins.save()
         context = {'success' : True}
         
 
-    return render(request, 'index.html', context)
+    return render(request, 'addTask.html', context)
 
 def tasks(request):
     allTasks = Task.objects.all()
     context = {'tasks' : allTasks}
     return render(request, 'tasks.html', context)
+
+
+
+class Edit(UpdateView): 
+    model = Task
+    template_name = 'edit.html'
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class Delete(DeleteView):
+    model = Task
+    template_name = 'delete.html'
+    success_url = reverse_lazy('tasks')
+
+
+class Details(DetailView):
+    model = Task
+    template_name = 'details.html'
